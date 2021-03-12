4e85669f6136ed0283cf7bd15f02fdbc945000b6
Luca <52624146+EzioTheDeadPoet@users.noreply.github.com>
2021-03-12 23:32:50 +0100

    Delete src directory

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { ConfigurationComponent } from './configuration-component';
-
-document.querySelector('h2#configuration')!
-  .insertAdjacentElement('afterend', new ConfigurationComponent().element);
